d99c06811d2fd690a30240bca85ad2c4aeac36ab
Martin Hanekom <martin.barend.hanekom@ericsson.com>
2022-10-24 15:48:45 +0200

    bullets

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bubbler
+
+bubbler zombie shooting game in C++ with SFML.
+
+### Build from source:
+
+[Install SFML](https://www.sfml-dev.org/download.php) then run:
+
+```
+make
+./main
+```
+
+Enjoy!
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 #include <SFML/Graphics.hpp>
 
 #define WIN_W 800
@@ -8,52 +9,45 @@
 
 #define HEALTH_RATIO 10
 #define P_HEALTH 100.0
+#define P_RADIUS 10.0
 #define P_SPEED 60.0
 #define GUN_W 3
 #define GUN_H 15
+#define GUN_COOLDOWN 0.2
+#define BULLET_W 5
+#define BULLET_H 1
+#define BULLET_SPEED 400.0
 
 float vAbs(sf::Vector2f vec) {
   return sqrt(vec.x * vec.x + vec.y * vec.y);
 }
 
-class Entity {
-  public:
-    float health;
-    sf::Vector2f pos;
-    sf::Color color;
+float vAngle(sf::Vector2f pos) {
+  sf::Vector2f unitPos(1, 0);
+  float angle = acos((pos.x * unitPos.x + pos.y * unitPos.y) / (vAbs(pos) * vAbs(unitPos)));
+  return angle * copysignf(1.0, pos.y);
+}
 
-    Entity(float health, sf::Color color, sf::Vector2f pos):
-      health(health), color(color), pos(pos) { }
-
-    float getRadius() {
-      return health / HEALTH_RATIO;
-    }
-
-    virtual void update(sf::Time dt) { }
-
-    virtual void draw(sf::RenderWindow window) { }
-};
-
-class Player: public Entity {
+class Player {
   public:
     sf::CircleShape body;
     sf::RectangleShape gun;
+    float health = P_HEALTH;
+    float radius = P_RADIUS;
+    float gunCooldown = 0;
+    sf::Vector2f pos;
+    sf::Color color;
 
-    Player(float health, sf::Color color, sf::Vector2f pos):
-      Entity(health, color, pos) {
-      body.setFillColor(color);
-      gun.setFillColor(sf::Color::White);
-      resize();
-    }
-
-    void resize() {
-      float radius = getRadius();
-      body.setPosition(pos);
+    Player(sf::Vector2f pos): pos(pos), color(sf::Color::Red) {
       body.setRadius(radius);
-      body.setOrigin(sf::Vector2f(radius, radius));
-      gun.setPosition(pos);
+      body.setOrigin(radius, radius);
+      body.setPosition(pos);
+      body.setFillColor(color);
+
       gun.setSize(sf::Vector2f(GUN_W, GUN_H));
-      gun.setOrigin(sf::Vector2f(-radius, 0));
+      gun.setOrigin(-radius, 0);
+      gun.setPosition(pos);
+      gun.setFillColor(sf::Color::White);
     }
 
     void update(float dt) {
@@ -72,21 +66,52 @@ class Player: public Entity {
     }
 
     void mouseMove(sf::Vector2f mousePos) {
-      float radius = getRadius();
-      float gunAngle = gun.getRotation() * M_PI / 180.0;
-      sf::Vector2f relPos = mousePos - pos - sf::Vector2f(radius * cos(gunAngle), radius * sin(gunAngle));
-      if (vAbs(relPos) > 2 * radius) {
-        //std::cout << "relPos: " << relPos.x << "," << relPos.y << std::endl;
-        sf::Vector2f unitPos(1, 0);
-        float angle = acos((relPos.x * unitPos.x + relPos.y * unitPos.y) / (vAbs(relPos) * vAbs(unitPos))) * 180 / M_PI;
-        angle = angle * copysignf(1.0, relPos.y) - 90.0;
-        gun.setRotation(angle);
+      sf::Vector2f dirPos = mousePos - getGunPosition();
+      if (vAbs(dirPos) > 2 * radius) {
+        gun.setRotation(vAngle(dirPos) * 180.0 / M_PI - 90.0);
       }
     }
 
     void draw(sf::RenderWindow &window) {
       window.draw(body);
       window.draw(gun);
+    }
+
+    sf::Vector2f getGunPosition() {
+      float gunAngle = gun.getRotation() * M_PI / 180.0;
+      return pos + sf::Vector2f(radius * cos(gunAngle), radius * sin(gunAngle));
+    }
+};
+
+class Bullet {
+  public:
+    sf::RectangleShape body;
+    sf::Vector2f pos;
+    sf::Color color;
+    float angle;
+
+    Bullet(sf::Vector2f pos, sf::Vector2f mousePos): pos(pos), color(sf::Color::Yellow) {
+      body.setSize(sf::Vector2f(BULLET_W, BULLET_H));
+      body.setOrigin(BULLET_W / 2, BULLET_H / 2);
+      body.setPosition(pos);
+      sf::Vector2f dirPos = mousePos - pos;
+      angle = vAngle(dirPos);
+      body.setRotation(angle * 180.0 / M_PI);
+    }
+
+    void update(float dt) {
+      pos.x += BULLET_SPEED * dt * cos(angle);
+      pos.y += BULLET_SPEED * dt * sin(angle);
+      //std::cout << "pos: " << pos.x << ", " << pos.y << std::endl;
+      body.setPosition(pos);
+    }
+
+    void draw(sf::RenderWindow &window) {
+      window.draw(body);
+    }
+
+    bool outOfBounds() {
+      return (pos.x < 0 || pos.x > WIN_W || pos.y < 0 || pos.y > WIN_H);
     }
 };
 
@@ -96,9 +121,11 @@ int main() {
   window.setVerticalSyncEnabled(true);
   window.setFramerateLimit(FS);
   sf::Clock clock;
+  float cooldown = 0;
 
   // Create player
-  Player player(P_HEALTH, sf::Color::Red, sf::Vector2f(WIN_W / 2, WIN_H / 2));
+  Player player(sf::Vector2f(WIN_W / 2, WIN_H / 2));
+  std::vector<Bullet> bullets;
 
   while (window.isOpen()) {
     sf::Event event;
@@ -110,15 +137,39 @@ int main() {
         case sf::Event::MouseMoved:
           player.mouseMove(sf::Vector2f(event.mouseMove.x, event.mouseMove.y));
           break;
+        case sf::Event::MouseButtonPressed:
+          bullets.push_back(Bullet(player.getGunPosition(), sf::Vector2f(sf::Mouse::getPosition(window))));
+          break;
         default:
           break;
       }
     }
-    player.update(clock.restart().asSeconds());
+
+    // update
+    float dt = clock.restart().asSeconds();
+    /*cooldown = (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) ? cooldown - dt : GUN_COOLDOWN;
+    if (cooldown <= 0) {
+      std::cout << "shoot" << std::endl;
+      bullets.push_back(Bullet(player.getGunPosition(), sf::Vector2f(sf::Mouse::getPosition(window))));
+    }*/
+    player.update(dt);
+    auto bullet = bullets.begin();
+    while (bullet != bullets.end()) {
+      bullet->update(dt);
+      if (bullet->outOfBounds()) {
+        bullet = bullets.erase(bullet);
+      } else {
+        bullet++;
+      }
+    }
+
+    // draw
     window.clear();
     player.draw(window);
+    for (auto &bullet : bullets) {
+      bullet.draw(window);
+    }
     window.display();
   }
-
   return 0;
 }
